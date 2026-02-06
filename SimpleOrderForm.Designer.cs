833e80ec9b48880292ec6d0a332d49b9257b3052
andrew <a.wachencko@gmail.com>
2026-02-06 10:10:26 +1000

    Обновил дизайн SimpleOrderForm (координаты и размеры)

---
+++ SimpleOrderForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MyManager
+{
+    partial class SimpleOrderForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            _textNumber = new TextBox();
+            _datePicker = new DateTimePicker();
+            _btnOk = new Button();
+            _btnCancel = new Button();
+            labelNum = new Label();
+            labelDate = new Label();
+            SuspendLayout();
+            // 
+            // _textNumber
+            // 
+            _textNumber.Location = new Point(195, 44);
+            _textNumber.Name = "_textNumber";
+            _textNumber.Size = new Size(208, 31);
+            _textNumber.TabIndex = 0;
+            // 
+            // _datePicker
+            // 
+            _datePicker.Format = DateTimePickerFormat.Short;
+            _datePicker.Location = new Point(195, 96);
+            _datePicker.Name = "_datePicker";
+            _datePicker.Size = new Size(208, 31);
+            _datePicker.TabIndex = 1;
+            // 
+            // _btnOk
+            // 
+            _btnOk.Location = new Point(306, 184);
+            _btnOk.Name = "_btnOk";
+            _btnOk.Size = new Size(97, 35);
+            _btnOk.TabIndex = 2;
+            _btnOk.Text = "ОК";
+            _btnOk.UseVisualStyleBackColor = true;
+            _btnOk.Click += _btnOk_Click;
+            // 
+            // _btnCancel
+            // 
+            _btnCancel.Location = new Point(197, 184);
+            _btnCancel.Name = "_btnCancel";
+            _btnCancel.Size = new Size(97, 35);
+            _btnCancel.TabIndex = 3;
+            _btnCancel.Text = "Отмена";
+            _btnCancel.UseVisualStyleBackColor = true;
+            _btnCancel.Click += _btnCancel_Click;
+            // 
+            // labelNum
+            // 
+            labelNum.AutoSize = true;
+            labelNum.Location = new Point(36, 48);
+            labelNum.Name = "labelNum";
+            labelNum.Size = new Size(130, 25);
+            labelNum.TabIndex = 1;
+            labelNum.Text = "Номер заказа:";
+            // 
+            // labelDate
+            // 
+            labelDate.AutoSize = true;
+            labelDate.Location = new Point(36, 102);
+            labelDate.Name = "labelDate";
+            labelDate.Size = new Size(110, 25);
+            labelDate.TabIndex = 0;
+            labelDate.Text = "Дата заказа:";
+            // 
+            // SimpleOrderForm
+            // 
+            AcceptButton = _btnOk;
+            CancelButton = _btnCancel;
+            ClientSize = new Size(448, 273);
+            Controls.Add(labelDate);
+            Controls.Add(labelNum);
+            Controls.Add(_btnCancel);
+            Controls.Add(_btnOk);
+            Controls.Add(_datePicker);
+            Controls.Add(_textNumber);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SimpleOrderForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Данные заказа";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private TextBox _textNumber;
+        private DateTimePicker _datePicker;
+        private Button _btnOk;
+        private Button _btnCancel;
+        private Label labelNum;
+        private Label labelDate;
+    }
+}
--- SimpleOrderForm.cs
+++ SimpleOrderForm.cs
@@ -1,69 +1,43 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyManager
 {
-    public class SimpleOrderForm : Form
+    public partial class SimpleOrderForm : Form
     {
-        private readonly TextBox _textNumber = new TextBox();
-        private readonly DateTimePicker _datePicker = new DateTimePicker();
-        private readonly Button _btnOk = new Button();
-        private readonly Button _btnCancel = new Button();
-
         public string OrderNumber { get; private set; } = "";
         public DateTime OrderDate { get; private set; } = DateTime.Now;
 
-        public SimpleOrderForm(OrderData data = null)
+        public SimpleOrderForm(OrderData? data = null)
         {
-            Text = "Данные заказа";
-            StartPosition = FormStartPosition.CenterParent;
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            ClientSize = new Size(360, 180);
-
-            var labelNumber = new Label { Text = "Номер заказа:", AutoSize = true, Location = new Point(20, 20) };
-            _textNumber.Location = new Point(20, 45);
-            _textNumber.Width = 300;
-
-            var labelDate = new Label { Text = "Дата заказа:", AutoSize = true, Location = new Point(20, 80) };
-            _datePicker.Location = new Point(20, 105);
-            _datePicker.Width = 200;
-            _datePicker.Format = DateTimePickerFormat.Short;
-
-            _btnOk.Text = "ОК";
-            _btnOk.Location = new Point(160, 140);
-            _btnOk.Click += OnOk;
-
-            _btnCancel.Text = "Отмена";
-            _btnCancel.Location = new Point(245, 140);
-            _btnCancel.Click += (s, e) => { DialogResult = DialogResult.Cancel; Close(); };
-
-            Controls.AddRange(new Control[] { labelNumber, _textNumber, labelDate, _datePicker, _btnOk, _btnCancel });
-            AcceptButton = _btnOk;
-            CancelButton = _btnCancel;
+            InitializeComponent();
 
             if (data != null)
             {
                 _textNumber.Text = data.Id;
-                _datePicker.Value = data.OrderDate == default ? DateTime.Now : data.OrderDate;
+                if (data.OrderDate != default) _datePicker.Value = data.OrderDate;
             }
         }
 
-        private void OnOk(object sender, EventArgs e)
+        private void _btnOk_Click(object sender, EventArgs e)
         {
-            string number = (_textNumber.Text ?? "").Trim();
-            if (string.IsNullOrWhiteSpace(number))
+            string num = _textNumber.Text.Trim();
+            if (string.IsNullOrWhiteSpace(num))
             {
                 MessageBox.Show("Введите номер заказа.");
                 return;
             }
 
-            OrderNumber = number;
+            OrderNumber = num;
             OrderDate = _datePicker.Value;
-            DialogResult = DialogResult.OK;
-            Close();
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void _btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
         }
     }
 }
